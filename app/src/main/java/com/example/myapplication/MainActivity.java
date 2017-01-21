0873cda7f41ac5fd6f65e1fb41f92d213973e791
Philippe Li <philo1196@gmail.com>
2017-01-21 16:05:43 -0500

    Added buttons and fixed android manifest

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,37 +1,26 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.Spinner;
 import android.widget.Toast;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 public class MainActivity extends AppCompatActivity {
 
-
 //------------------Reminder---------------------------
-    NotificationCompat.Builder notification ;
+    NotificationCompat.Builder notification;
     private static final int uniqueID = 000000;
 //-----------------------------------------------------
 
-
-
-    CalendarView calendar;
-    Button toDoListButton;
+    private CalendarView calendar;
+    private Button toDoListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view,int year, int month, int dayOfMonth){
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
             }
         });
@@ -56,60 +45,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         //------------------Reminders--------------------------------------
-        notification =   new NotificationCompat.Builder(this);
-
+        notification = new NotificationCompat.Builder(this);
 
 
         notification.setAutoCancel(true);
         //--------------------------------------------------------
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
-
     }
 
     @Override
     public void onStop() {
         super.onStop();
-
-
     }
 
-
-
-    public void fireNotification(View view){
+    public void fireNotification(View view) {
         notification.setSmallIcon(R.mipmap.ic_launcher);
         notification.setContentTitle("My notification");
         notification.setContentText("Hello World!");
@@ -117,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         //for displaying the time associated with a notification
         notification.setWhen(System.currentTimeMillis());
 
-// To trigger notifications at certain times use AlarmManager
+        // To trigger notifications at certain times use AlarmManager
 
         // Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(this, MainActivity.class);
@@ -125,13 +79,13 @@ public class MainActivity extends AppCompatActivity {
 
         //  notification.setContentIntent(pendingIntent);
         // The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-// Adds the back stack for the Intent (but not the Intent itself)
+        // Adds the back stack for the Intent (but not the Intent itself)
         stackBuilder.addParentStack(MainActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
+        // Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(resultIntent);
         PendingIntent resultPendingIntent =
                 stackBuilder.getPendingIntent(
@@ -141,8 +95,6 @@ public class MainActivity extends AppCompatActivity {
         notification.setContentIntent(resultPendingIntent);
 
 
-
-
         NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         // ID to update notification later
         nm.notify(uniqueID, notification.build());
@@ -150,14 +102,4 @@ public class MainActivity extends AppCompatActivity {
         //--------------------------------------------------------
 
     }
-
-
-
-
-
-
-
-
-
-
 }
--- app/src/main/java/com/example/myapplication/ToDoList.java
+++ app/src/main/java/com/example/myapplication/ToDoList.java
@@ -5,24 +5,46 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoList extends AppCompatActivity {
-    ImageButton addToDo;
+    private ImageButton addToDo;
+    private List<String> toDoEntries;
+    private ListView toDoListEntries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
 
+        generateToDoList();
+
         addToDo = (ImageButton) findViewById(R.id.addToDoEntry);
         addToDo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), ToDoAdd.class);
-                startActivity(intent);
+                /*Intent intent = new Intent(v.getContext(), ToDoAdd.class);
+                startActivity(intent);*/
             }
         });
+    }
+
+    private void generateToDoList() {
+        toDoListEntries = (ListView) findViewById(R.id.toDoListEntries);
+
+        toDoEntries = new ArrayList<>();
+        toDoEntries.add("Assignment due tomorrow");
+        toDoEntries.add("Quiz next Friday");
+
+        ArrayAdapter<String> stringAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, toDoEntries);
+
+        toDoListEntries.setAdapter(stringAdapter);
     }
 }
