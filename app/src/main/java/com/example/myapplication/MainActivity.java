f5d02a53891b9f86e5ce6c6a067a740eef9bf7ba
Philippe Li <philo1196@gmail.com>
2017-01-21 12:17:02 -0500

    Added To Do List activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     CalendarView calendar;
+    Button toDoListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         calendar = (CalendarView) findViewById(R.id.calendar);
-
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view,int year, int month, int dayOfMonth){
                 Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
             }
         });
+
+        toDoListButton = (Button) findViewById(R.id.toDoListButton);
+        toDoListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ToDoList.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
-    
 }
---
+++ app/src/main/java/com/example/myapplication/ToDoList.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class ToDoList extends AppCompatActivity {
+    ImageButton addToDo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_list);
+
+        addToDo = (ImageButton) findViewById(R.id.addToDoEntry);
+        addToDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ToDoAdd.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
